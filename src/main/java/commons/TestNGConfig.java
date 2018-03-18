c9e25e32d32632257370b4a2f9351dee500d83e9
maysq <m2991992>
2018-03-18 10:31:39 +0200

    add browser parameter to TestNG

--- src/main/java/commons/TestNGConfig.java
+++ src/main/java/commons/TestNGConfig.java
@@ -1,21 +1,38 @@
 package commons;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class TestNGConfig {
 
     public static WebDriver driver;
     public static String baseURL= "http://10.0.0.139/sedcowebserver";
-
+@Parameters ("browser")
     @BeforeTest
-    public void setUp(){
-
+    public void setUp(String browser){
+switch (browser){
+    case "chrome":{
         ChromeDriverManager.getInstance ().setup ();
         driver= new ChromeDriver ();
+        break;
+    }
+    case "firefox":
+    {
+        FirefoxDriverManager.getInstance ().setup ();
+        driver= new FirefoxDriver ();
+        break;
+    }
+    default:{
+        ChromeDriverManager.getInstance ().setup ();
+        driver= new ChromeDriver ();
+    }
+}
         driver.navigate ().to (baseURL);
         driver.manage ().window ().maximize ();
     }
@@ -23,7 +40,7 @@ public class TestNGConfig {
 
     @AfterTest
     public void tearDown(){
-        //driver.quit ();
+        driver.quit ();
     }
 
 }
