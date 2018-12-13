31b5e5ab06260366fa66f8f639310bda5a617321
maysq <m2991992>
2018-12-13 13:06:09 +0200

    add tests

--- src/test/java/pageObjects/LogInPage.java
+++ src/test/java/pageObjects/LogInPage.java
@@ -5,40 +5,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LogIn extends TestNGConfig {
+public class LogInPage extends TestNGConfig {
 
-    WebDriverWait webDriverWait= new WebDriverWait (driver, 10);;
+    WebDriverWait webDriverWait = new WebDriverWait (driver, 10);
+
 
     /* Define the element locators*/
-    public static By usrnameTxt= By.id("txtUserName");
-    public static By passwordTxt= By.id("txtPassword");
-    public static By logInBtn= By.cssSelector ("button.ladda-button.ladda-button-demo.btn.btn-primary[type=submit]");
+    public static By usrnameTxt = By.id ("txtUserName");
+    public static By passwordTxt = By.id ("txtPassword");
+    public static By logInBtn = By.cssSelector ("button.ladda-button.ladda-button-demo.btn.btn-primary[type=submit]");
+
     //Log in info
 
-    public LogIn(WebDriver driver){
-
-        this.driver=driver;
-
+    //Constructor
+    public LogInPage (WebDriver driver) {
+        this.driver = driver;
     }
 
+    public void enterUserName (String username) {
+        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (usrnameTxt)).sendKeys (username);
+    }
 
-    public void enterUserName(String username){
-        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (usrnameTxt)).sendKeys(username);
+    public void enterPassword (String password) {
+        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (passwordTxt)).sendKeys (password);
+    }
+
+    public void clickLogin ( ) {
+        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (logInBtn)).click ( );
+    }
+public void verifySuccessfulLogin(String username){
+        HomePage home= new HomePage (driver);
+        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (home.userBtn)).getText ();
+        Assert.assertEquals (webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (home.userBtn)).getText (), username);
 }
-    public void enterPassword(String password) {
-        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (passwordTxt)).sendKeys(password);
-    }
-    public void clickLogin(){
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated (logInBtn)).click ();
-    }
-
-    public void loginOperation(){
-        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (By.id("txtUserName"))).sendKeys("root");
-        webDriverWait.until (ExpectedConditions.visibilityOfElementLocated (passwordTxt)).sendKeys("root");
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated (logInBtn)).click ();
-    }
 
 }
